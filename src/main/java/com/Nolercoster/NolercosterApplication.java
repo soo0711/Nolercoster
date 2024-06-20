33a1022d6bc897d80790b3bc2d99f02e44eab822
soo0711 <k300hys@naver.com>
2024-06-20 15:05:40 +0900

    환경세팅

---
+++ src/main/java/com/Nolercoster/NolercosterApplication.java
@@ -0,0 +1,13 @@
+package com.Nolercoster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NolercosterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NolercosterApplication.class, args);
+	}
+
+}
